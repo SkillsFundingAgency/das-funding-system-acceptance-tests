2adad1a65e6fc59f61a160dac602151707d382e8
jack-cloud-platform <38191888+jack-cloud-platform@users.noreply.github.com>
2022-08-11 14:29:57 +0100

    Added initial YAML pipeline

--- src/SFA.DAS.Funding.SystemAcceptanceTests/Configurator.cs
+++ src/SFA.DAS.Funding.SystemAcceptanceTests/Configurator.cs
@@ -10,7 +10,6 @@ namespace SFA.DAS.Funding.SystemAcceptanceTests
             return new ConfigurationBuilder()
                 .AddEnvironmentVariables()
                 .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("local.settings.json", optional: true)
                 .Build();
         }
 
