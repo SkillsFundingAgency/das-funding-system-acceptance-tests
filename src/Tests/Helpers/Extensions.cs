2adc58be892a87b656f923b3d2d4606d3928e133
Satya Sanikommu <satya.sanikommu@digital.education.gov.uk>
2022-08-18 15:58:24 +0000

    FPAY-14 test automation
    * initial commit with a sample steps
    
    * Updated the initial test
    
    * Completed FPAY-14 first scenario
    
    * Completed FPAY-14 test scenario
    
    * Addressed review comments and updated the feature to the discussed formart
    
    * add ncrunch file to gitignore
    
    * Completed FPAY-14 first scenario
    
    * Use values table for asserting funding periods, fix spelling errors
    
    * Config tree-shake
    
    Co-authored-by: Arvinth Seran <arvinth.seran@digital.education.gov.uk>
    Co-authored-by: ruslan <ruslanss@gmail.com>

--- src/Tests/Helpers/Extensions.cs
+++ src/Tests/Helpers/Extensions.cs
@@ -2,68 +2,15 @@ namespace SFA.DAS.Funding.SystemAcceptanceTests.Helpers;
 
 public static class Extensions
 {
-    public static void ShouldHaveCorrectFundingPeriods(this List<DeliveryPeriod> periods, int numberOfInstalments, short firstDeliveryPeriod, short firstDeliveryAcademicYear)
+    public static void ShouldHaveCorrectFundingPeriods(this List<DeliveryPeriod> actual, List<(byte Period, short AcademicYear, byte Month)> expected)
     {
-        if (firstDeliveryPeriod > 12) throw new ArgumentException("Period can't be greater than 12 can it?", nameof(firstDeliveryPeriod));
-
-        var expectedPeriod = firstDeliveryPeriod;
-        var expectedAcademicYear = firstDeliveryAcademicYear;
-
-        List<(short Period, short AcademicYear)> expected = new();
-
-        for (var i = 0; i < numberOfInstalments; i++)
+        actual.Count.Should().Be(expected.Count);
+        
+        for (var i = 0; i < expected.Count; i++)
         {
-            expected.Add(new ValueTuple<short, short>(expectedPeriod, expectedAcademicYear));
-            if (expectedPeriod == 12)
-            {
-                expectedPeriod = 1;
-                expectedAcademicYear = GetNextAcademicYear(expectedAcademicYear);
-            }
-            else expectedPeriod++;
+            actual[i].Period.Should().Be(expected[i].Period, $"Expected period #{i} to be {expected[i].Period}/{expected[i].AcademicYear}");
+            actual[i].AcademicYear.Should().Be(expected[i].AcademicYear, $"Expected period #{i} to be {expected[i].Period}/{expected[i].AcademicYear}");
+            actual[i].CalendarMonth.Should().Be(expected[i].Month, $"Expected Calendar Month in period #{i} to be {expected[i].Month}");
         }
-
-        var actual  = periods
-            .Select(c => new { c.Period, c.AcademicYear})
-            .AsEnumerable()
-            .Select(c => new Tuple<short, short>(c.Period, c.AcademicYear))
-            .ToList();
-
-        actual.Should().BeEquivalentTo(expected);
-    }
-
-    private static short GetNextAcademicYear(short expectedAcademicYear)
-    {
-        var firstPart = expectedAcademicYear / 100 + 1;
-        var secondPart = expectedAcademicYear % 100 + 1;
-        return (short)(firstPart * 100 + secondPart);
-    }
-
-    public static void ShouldHaveCorrectFundingCalendarMonths(this List<DeliveryPeriod> periods, int numberOfInstalments, short firstCalendarPeriodMonth, short firstCalendarPeriodYear)
-    {
-        if (firstCalendarPeriodMonth > 12) throw new ArgumentException("A Month number can't be greater than 12 can it?", nameof(firstCalendarPeriodMonth));
-
-        var expectedMonth = firstCalendarPeriodMonth;
-        var expectedYear = firstCalendarPeriodYear;
-
-        List<(short CalendarMonth, short CalenderYear)> expected = new();
-
-        for (var i = 0; i < numberOfInstalments; i++)
-        {
-            expected.Add(new ValueTuple<short, short>(expectedMonth, expectedYear));
-            if (expectedMonth == 12)
-            {
-                expectedMonth = 1;
-                expectedYear++;
-            }
-            else expectedMonth++;
-        }
-
-        var actual = periods
-            .Select(c => new { c.CalendarMonth, c.CalenderYear })
-            .AsEnumerable()
-            .Select(c => new Tuple<short, short>(c.CalendarMonth, c.CalenderYear))
-            .ToList();
-
-        actual.Should().BeEquivalentTo(expected);
     }
 }
--- src/Tests/StepDefinitions/CalculateEarningsForLearningPaymentsStepDefinitions.cs
+++ src/Tests/StepDefinitions/CalculateEarningsForLearningPaymentsStepDefinitions.cs
@@ -6,55 +6,44 @@ namespace SFA.DAS.Funding.SystemAcceptanceTests.StepDefinitions;
 public class CalculateEarningsForLearningPaymentsStepDefinitions
 {
     private readonly ScenarioContext _context;
-    private ApprenticeshipCreatedEvent _apprenticeshipCreatedEvent;
-    private EarningsGeneratedEvent _earnings;
+    private ServiceBusMessageHelper _messageHelper;
 
     public CalculateEarningsForLearningPaymentsStepDefinitions(ScenarioContext context)
     {
         _context = context;
+        _messageHelper = new ServiceBusMessageHelper(_context);
     }
 
-    [Given(@"An apprenticeship is created with (.*), (.*), (.*)")]
-    public async Task GivenAnApprenticeshipIsCreatedWith(decimal agreedPrice, DateTime actualStartDate,
-        DateTime plannedEndDate)
+    [Given(@"an apprenticeship has a start date of (.*), a planned end date of (.*), and an agreed price of £(.*)")]
+    public void AnApprenticeshipIsCreatedWith(DateTime startDate, DateTime plannedEndDate, decimal agreedPrice)
     {
-        var fixture = new Fixture();
-        _apprenticeshipCreatedEvent = fixture.Build<ApprenticeshipCreatedEvent>()
-            .With(_ => _.AgreedPrice, agreedPrice)
-            .With(_ => _.ActualStartDate, actualStartDate)
-            .With(_ => _.PlannedEndDate, plannedEndDate)
-            .With(_ => _.Uln, fixture.Create<long>().ToString)
-            .Create();
-
-        await _context.Get<TestMessageBus>().Publish(_apprenticeshipCreatedEvent);
+        _messageHelper.CreateApprenticeshipCreatedMessageWithCustomValues(startDate, plannedEndDate, agreedPrice);
     }
 
-    [Then(@"Earnings results are published with calculated (.*), (.*), (.*), R(.*)-(.*), (.*)/(.*)")]
-    public async Task ThenEarningsResultsArePublishedWithCalculated(decimal adjustedAgreedPrice, decimal learningAmount, int numberOfInstalments, short firstDeliveryPeriod, short firstDeliveryAcademicYear, short firstCalendarPeriodMonth, short firstCalendarPeriodYear)
+    [When(@"the apprenticeship commitment is approved")]
+    public async Task TheApprenticeshipCommitmentIsApproved()
     {
-        await WaitHelper.WaitForIt(() => EarningsGeneratedEventHandler.ReceivedEvents.Any(), "Failed to find published event");
-
-        _earnings = EarningsGeneratedEventHandler.ReceivedEvents.First();
-        _earnings.FundingPeriods.Should().HaveAdjustedAgreedPriceOf(adjustedAgreedPrice);
-
-        var firstFundingPeriod = _earnings.FundingPeriods.First();
-        firstFundingPeriod.DeliveryPeriods.Should().HaveCount(numberOfInstalments);
-        firstFundingPeriod.AgreedPrice.Should().Be(adjustedAgreedPrice);
-        firstFundingPeriod.DeliveryPeriods.ForEach(dp => dp.LearningAmount.Should().Be(learningAmount));
-
-        firstFundingPeriod.DeliveryPeriods.ShouldHaveCorrectFundingPeriods(numberOfInstalments, firstDeliveryPeriod, firstDeliveryAcademicYear);
-        firstFundingPeriod.DeliveryPeriods.ShouldHaveCorrectFundingCalendarMonths(numberOfInstalments, firstCalendarPeriodMonth, firstCalendarPeriodYear);
+        await _messageHelper.PublishApprenticeshipApprovedMessage();
+        await _messageHelper.ReadEarningsGeneratedMessage();
     }
 
-    [Then(@"correct Uln, EmployerId, ProviderId, TransferSenderEmployerId, StartDate, TrainingCode, EmployerType information")]
-    public void ThenCorrectUlnEmployerIdProviderIdTransferSenderEmployerIdStartDateTrainingCodeEmployerTypeInformation()
+    [Then(@"80% of the agreed price is calculated as total on-program payment which is divided equally into number of planned months (.*)")]
+    public void VerifyInstalmentAmountIsCalculatedEquallyIntoAllEarningMonths(decimal instalmentAmount)
     {
-        _earnings.FundingPeriods.First().Uln.Should().Be(Convert.ToInt64(_apprenticeshipCreatedEvent.Uln));
-        _earnings.FundingPeriods.First().EmployerId.Should().Be(_apprenticeshipCreatedEvent.EmployerAccountId);
-        _earnings.FundingPeriods.First().ProviderId.Should().Be(_apprenticeshipCreatedEvent.UKPRN);
-        _earnings.FundingPeriods.First().TransferSenderEmployerId.Should().Be(_apprenticeshipCreatedEvent.FundingEmployerAccountId);
-        _earnings.FundingPeriods.First().StartDate.Should().Be(_apprenticeshipCreatedEvent.ActualStartDate);
-        _earnings.FundingPeriods.First().TrainingCode.Should().Be(_apprenticeshipCreatedEvent.TrainingCode);
-        _earnings.FundingPeriods.First().EmployerType.Should().Be(_apprenticeshipCreatedEvent.FundingType == FundingType.NonLevy ? EmployerType.NonLevy : EmployerType.Levy);
+        _context.Get<FundingPeriod>().DeliveryPeriods.ForEach(dp => dp.LearningAmount.Should().Be(instalmentAmount));
+    }
+
+    [Then(@"the planned number of months must be the number of months from the start date to the planned end date (.*)")]
+    public void VerifyThePlannedDurationMonthsWithinTheEarningsGenerated(short numberOfInstalments)
+    {
+        _context.Get<FundingPeriod>().DeliveryPeriods.Should().HaveCount(numberOfInstalments);
+    }
+
+    [Then(@"the delivery period for each instalment must be the delivery period from the collection calendar with a matching calendar month/year")]
+    public void ThenTheDeliveryPeriodForEachInstalmentMustBeTheDeliveryPeriodFromTheCollectionCalendarWithAMatchingCalendarMonthYear(Table table)
+    {
+        var deliveryPeriods = _context.Get<FundingPeriod>().DeliveryPeriods;
+
+        deliveryPeriods.ShouldHaveCorrectFundingPeriods(table.ToExpectedPeriods());
     }
 }
