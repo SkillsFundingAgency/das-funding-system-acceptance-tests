0fd0d33fdc582dd8f013abc633f43b283b362d53
ShakilUmar <umar.shakil@digital.education.gov.uk>
2022-09-15 14:20:52 +0100

    added logic to point to different test environments

--- src/SFA.DAS.Funding.SystemAcceptanceTests/Configurator.cs
+++ src/SFA.DAS.Funding.SystemAcceptanceTests/Configurator.cs
@@ -5,6 +5,33 @@ namespace SFA.DAS.Funding.SystemAcceptanceTests
 {
     public static class Configurator
     {
+
+        private readonly static string? EnvironmentName;
+        private readonly static bool IsVstsExecution;
+        private readonly static IConfigurationRoot _hostingConfig;
+
+        static Configurator()
+        {
+            _hostingConfig = InitializeHostingConfig();
+            EnvironmentName = GetEnvironmentName();
+            IsVstsExecution = TestsExecutionInVsts();
+        }
+
+        private static IConfigurationRoot InitializeHostingConfig() => ConfigurationBuilder()
+                .AddJsonFile("appsettings.ProjectConfig.json", true)
+                .AddEnvironmentVariables()
+                .Build();
+
+        private static IConfigurationBuilder ConfigurationBuilder() => new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory());
+
+        private static bool TestsExecutionInVsts() => !string.IsNullOrEmpty(GetAgentMachineName());
+
+        private static string? GetAgentMachineName() => GetHostingConfigSection("AGENT_MACHINENAME");
+
+        private static string? GetEnvironmentName() => IsVstsExecution ? GetHostingConfigSection("RELEASE_ENVIRONMENTNAME") : GetHostingConfigSection("EnvironmentName");
+        private static string? GetHostingConfigSection(string name) => _hostingConfig.GetSection(name)?.Value;
+
         public static FundingConfig GetConfiguration()
         {
             var configuration = new FundingConfig();
@@ -20,7 +47,7 @@ namespace SFA.DAS.Funding.SystemAcceptanceTests
             return new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.ProjectConfig.json", optional: true)
-                .AddUserSecrets("FundingTestExecutionSecrets")
+                .AddUserSecrets($"Funding_{EnvironmentName}_ExecutionSecrets")
                 .AddEnvironmentVariables()
                 .Build();
         }
