cdecc9374842167f1aa1746cbfd2604fbb2ead15
dinathain <55487881+dinathain@users.noreply.github.com>
2023-09-05 15:24:18 +0100

    Manage ServiceBus resources before and after test run

---
+++ src/SFA.DAS.Funding.SystemAcceptanceTests/Helpers/AzureServiceBusClient.cs
@@ -0,0 +1,113 @@
+﻿using Azure.Identity;
+using Azure.Messaging.ServiceBus.Administration;
+
+namespace SFA.DAS.Funding.SystemAcceptanceTests.Helpers
+{
+    internal class AzureServiceBusClient
+    {
+        private readonly ServiceBusAdministrationClient _administrationClient;
+
+        public AzureServiceBusClient(string azureServiceBusNamespace)
+        {
+            _administrationClient = new ServiceBusAdministrationClient(azureServiceBusNamespace, new DefaultAzureCredential());
+        }
+
+        internal async Task CreateSubscriptionAsync(string subscriptionName, string topicName, string destinationQueueName, List<string>? filterEventTypes = null)
+        {
+            var subscriptionExists = await _administrationClient.SubscriptionExistsAsync(topicName, subscriptionName);
+            if (!subscriptionExists)
+            {
+                // Create subscription
+                var subscriptionsOptions = new CreateSubscriptionOptions(topicName, subscriptionName) { ForwardTo = destinationQueueName };
+                await _administrationClient.CreateSubscriptionAsync(subscriptionsOptions);
+
+                if (filterEventTypes != null && filterEventTypes.Count > 0)
+                {
+                    // Remove any default rules
+                    await _administrationClient.DeleteRuleAsync(topicName, subscriptionName, CreateRuleOptions.DefaultRuleName);
+
+                    // Create filters
+                    var sqlExpression = "[NServiceBus.EnclosedMessageTypes] LIKE '%" + string.Join("%' OR [NServiceBus.EnclosedMessageTypes] LIKE '%", filterEventTypes);
+                    await _administrationClient.CreateRuleAsync(topicName, subscriptionName, new CreateRuleOptions
+                    {
+                        Name = $"{new Guid()}",
+                        Filter = new SqlRuleFilter(sqlExpression)});
+                }
+            }
+            else
+            {
+                Assert.Fail(
+                    $"Attempted to create subscription with name {subscriptionName} but already exists. Time: {DateTime.Now:G}.");
+            }
+        }
+
+        internal async Task CreateQueueAsync(string queueName)
+        {
+            var queueExists = await _administrationClient.QueueExistsAsync(queueName);
+            if (!queueExists)
+            {
+                await _administrationClient.CreateQueueAsync(new CreateQueueOptions(queueName)
+                {
+                    DefaultMessageTimeToLive = TimeSpan.FromHours(4) // Prevents queue from filling up if it doesn't get deleted successfully after test run
+                });
+            }
+            else
+            {
+                Assert.Fail($"Attempted to create queue with name {queueName} but already exists. Time: {DateTime.Now:G}.");
+            }
+
+            //var data = new object()
+            //{
+
+            //};
+            //var stringContent = data != null
+            //    ? new StringContent(JsonConvert.SerializeObject(data), System.Text.Encoding.UTF8, "application/json")
+            //    : null;
+            
+            //var apiVersion = _config.ApiVersion;
+            //var subscriptionId = _config.SubscriptionId;
+            //var resourceGroupName = _config.ServiceBusResourceGroupName;
+            //var resourceProviderNamespace = "Microsoft.ServiceBus";
+            //var parentResourcePath = "";
+            //var resourceType = "";
+            //var resourceName = "";
+
+            //var url =
+            //    $"https://management.azure.com/subscriptions/{subscriptionId}/resourcegroups/{resourceGroupName}/providers/{resourceProviderNamespace}/{parentResourcePath}/{resourceType}/{resourceName}?api-version={apiVersion}";
+
+            //var requestMessage = new HttpRequestMessage(HttpMethod.Post, url);
+            //requestMessage.Content = stringContent;
+
+            //var response = await _httpClient.SendAsync(requestMessage).ConfigureAwait(false);
+            //var json = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            //response.EnsureSuccessStatusCode();
+        }
+        internal async Task DeleteSubscriptionAsync(string subscriptionName, string topicName, string destinationQueueName)
+        {
+            var subscriptionExists = await _administrationClient.SubscriptionExistsAsync(topicName, subscriptionName);
+            if (subscriptionExists)
+            {
+                await _administrationClient.DeleteSubscriptionAsync(topicName, subscriptionName);
+            }
+            else
+            {
+                Assert.Fail(
+                    $"Attempted to delete subscription with name {subscriptionName} but does not exist. Time: {DateTime.Now:G}.");
+            }
+        }
+
+        internal async Task DeleteQueueAsync(string queueName)
+        {
+            var queueExists = await _administrationClient.QueueExistsAsync(queueName);
+            if (queueExists)
+            {
+                await _administrationClient.DeleteQueueAsync(queueName);
+            }
+            else
+            {
+                Assert.Fail($"Attempted to delete queue with name {queueName} but does not exist. Time: {DateTime.Now:G}.");
+            }
+        }
+    }
+}
--- src/SFA.DAS.Funding.SystemAcceptanceTests/Hooks/ServiceBusEndpointHook.cs
+++ src/SFA.DAS.Funding.SystemAcceptanceTests/Hooks/ServiceBusEndpointHook.cs
@@ -1,12 +1,36 @@
-﻿using SFA.DAS.Funding.SystemAcceptanceTests.Infrastructure.Configuration;
+﻿using SFA.DAS.Funding.SystemAcceptanceTests.Helpers;
+using SFA.DAS.Funding.SystemAcceptanceTests.Infrastructure.Configuration;
 
 namespace SFA.DAS.Funding.SystemAcceptanceTests.Hooks
 {
     [Binding]
     public class ServiceBusEndpointHook
     {
+        private readonly string _queueName;
+        private readonly string _subscriptionName;
+        private readonly string _topicEndpointName;
+        private readonly string _sharedServiceBusFqdn;
+        private readonly List<string> _eventTypes;
+        public ServiceBusEndpointHook()
+        {
+            var config = Configurator.GetConfiguration();
+            _queueName = config.FundingSystemAcceptanceTestQueue;
+            _sharedServiceBusFqdn = config.SharedServiceBusFqdn;
+            _topicEndpointName = config.SharedServiceBusTopicEndpoint;
+            _subscriptionName = config.FundingSystemAcceptanceTestSubscription;
+            _eventTypes = config.EventTypes.Split(", ").ToList();
+        }
+
+        [BeforeTestRun]
+        public async Task SetUpSubscription()
+        {
+            var azureClient = new AzureServiceBusClient(_sharedServiceBusFqdn);
+            await azureClient.CreateQueueAsync(_queueName);
+            await azureClient.CreateSubscriptionAsync(_subscriptionName, _topicEndpointName, _queueName, _eventTypes);
+        }
+        
         [BeforeScenario(Order = 2)]
-        public static async Task StartEndpoint(ScenarioContext context)
+        public async Task StartEndpoint(ScenarioContext context)
         {
             var config = context.Get<FundingConfig>();
 
@@ -27,6 +51,14 @@ namespace SFA.DAS.Funding.SystemAcceptanceTests.Hooks
 
            var pv2TestMessageBus = context.Get<TestMessageBus>(TestMessageBusKeys.Pv2);
            pv2TestMessageBus?.Stop();
+        }
+
+        [AfterTestRun]
+        public async Task TearDownSubscription()
+        {
+            var azureClient = new AzureServiceBusClient(_sharedServiceBusFqdn);
+            await azureClient.DeleteQueueAsync(_queueName);
+            await azureClient.DeleteSubscriptionAsync(_subscriptionName, _topicEndpointName, _queueName);
         }
     }
 }
