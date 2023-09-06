e44ed28ee1af8ba9b69fecec4107031f3e195cf5
dinathain <55487881+dinathain@users.noreply.github.com>
2023-09-06 13:45:07 +0100

    Remove unused NServiceBusLicese config

--- src/SFA.DAS.Funding.SystemAcceptanceTests.Infrastructure/Configuration/FundingConfig.cs
+++ src/SFA.DAS.Funding.SystemAcceptanceTests.Infrastructure/Configuration/FundingConfig.cs
@@ -3,7 +3,6 @@
 public class FundingConfig
 {
     public string SharedServiceBusFqdn { get; set; } = "<not set>";
-    public string NServiceBusLicense { get; set; } = "<not set>";
     public string SharedServiceBusTopicEndpoint { get; set; } = "<not set>";
     public string LearningTransportStorageDirectory { get; set; } = "<not set>";
     public string EarningsEntityApi_BaseUrl { get; set; } = "<not set>";
