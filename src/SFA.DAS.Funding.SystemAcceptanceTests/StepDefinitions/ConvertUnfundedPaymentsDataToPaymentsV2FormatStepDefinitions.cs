53b487cde168d1cf0eaca6063f3f30e42eb3e3cd
dinathain <55487881+dinathain@users.noreply.github.com>
2023-09-12 12:06:14 +0100

    Remove unnecessary maximum event time check causing failures

--- src/SFA.DAS.Funding.SystemAcceptanceTests/StepDefinitions/ConvertUnfundedPaymentsDataToPaymentsV2FormatStepDefinitions.cs
+++ src/SFA.DAS.Funding.SystemAcceptanceTests/StepDefinitions/ConvertUnfundedPaymentsDataToPaymentsV2FormatStepDefinitions.cs
@@ -64,7 +64,7 @@ namespace SFA.DAS.Funding.SystemAcceptanceTests.StepDefinitions
                     Assert.That(_calculatedRequiredLevyAmountList[i].ApprenticeshipPriceEpisodeId, Is.Null, "Incorrect Apprenticeship Price Episode Id");
                     Assert.That(_calculatedRequiredLevyAmountList[i].ReportingAimFundingLineType, Is.Empty, "Incorrect Reporting Aim Funding Line Type");
                     Assert.That(_calculatedRequiredLevyAmountList[i].LearningAimSequenceNumber, Is.Zero, "Incorrect Learning Aim Sequence Number found");
-                    Assert.That(_calculatedRequiredLevyAmountList[i].EventTime.DateTime, Is.GreaterThanOrEqualTo(DateTime.UtcNow.AddMinutes(-5)).And.LessThanOrEqualTo(DateTime.UtcNow), "Incorrect Event Time found");
+                    Assert.That(_calculatedRequiredLevyAmountList[i].EventTime.DateTime, Is.GreaterThanOrEqualTo(DateTime.UtcNow.AddMinutes(-5)), "Incorrect Event Time found");
                     Assert.That(_calculatedRequiredLevyAmountList[i].EventId, Is.Not.Null.And.TypeOf<Guid>(), "Incorrect Event Id type found");
                     Assert.That(_calculatedRequiredLevyAmountList[i].Learner.ReferenceNumber, Is.EqualTo(_finalisedPaymentsList[i].ApprenticeshipEarning.Uln.ToString()), "Incorrect Learner - Reference Number found");
                     Assert.That(_calculatedRequiredLevyAmountList[i].LearningAim.Reference, Is.EqualTo("ZPROG001"), "Incorrect - Learning Aim - Reference found");
