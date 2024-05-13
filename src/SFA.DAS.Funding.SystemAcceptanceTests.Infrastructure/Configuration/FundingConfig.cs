d53dabb297d15027f6c7820895ef984811502c11
tom-gough <tom.gough.tgsd@outlook.com>
2024-05-13 13:54:16 +0100

    new queue config, waiting on earnings ticket to complete

--- src/SFA.DAS.Funding.SystemAcceptanceTests.Infrastructure/Configuration/FundingConfig.cs
+++ src/SFA.DAS.Funding.SystemAcceptanceTests.Infrastructure/Configuration/FundingConfig.cs
@@ -14,6 +14,7 @@ public class FundingConfig
     public string FundingSystemAcceptanceTestSubscription { get; set; } = "<not set>";
     public string ReleasePaymentsEventHandlersQueue { get; set; } = "<not set>";
     public string PriceChangeApprovedEventHandlersQueue { get; set; } = "<not set>";
-    public string Pv2ServiceBusFqdn { get; set; } = "<not set>";
+    public string StartDateChangeApprovedEventHandlersQueue { get; set; } = "<not set>";
+	public string Pv2ServiceBusFqdn { get; set; } = "<not set>";
     public string Pv2FundingSourceQueue { get; set; } = "<not set>";
 }
