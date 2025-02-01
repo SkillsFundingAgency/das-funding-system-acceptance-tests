2fd62f458377bc68a2ed6c57cc3af7021c611f8d
tom-gough <tom.gough.tgsd@outlook.com>
2025-02-01 15:44:47 +0000

    FLP-1022 tidy up wire mock service hooks

---
+++ src/SFA.DAS.Funding.SystemAcceptanceTests/Hooks/LocalWireMockServerHooks.cs
@@ -0,0 +1,28 @@
+﻿using WireMock.Server;
+
+namespace SFA.DAS.Funding.SystemAcceptanceTests.Hooks;
+
+[Binding]
+public static class LocalWireMockServerHooks
+{
+    private static WireMockServer? _wireMockServer;
+
+    [BeforeTestRun]
+    public static void StartLocalWireMockServer()
+    {
+        if (Configurator.EnvironmentName == "LOCAL")
+        {
+            var localWireMockUrl = new Uri(Configurator.GetConfiguration().WireMockBaseUrl);
+            if (localWireMockUrl.Host != "localhost")
+                throw new ArgumentException("WireMockBaseUrl must be localhost when running tests against local environment.");
+
+            _wireMockServer = WireMockServer.Start(localWireMockUrl.Port);
+        }
+    }
+
+    [AfterTestRun]
+    public static void StopLocalWireMockServer()
+    {
+        _wireMockServer?.Stop();
+    }
+}
