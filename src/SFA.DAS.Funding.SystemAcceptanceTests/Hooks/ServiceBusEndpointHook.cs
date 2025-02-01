ac4454ca6a6795ae26de4b703201cb6aba7d9b49
tom-gough <tom.gough.tgsd@outlook.com>
2025-02-01 15:45:41 +0000

    FLP-1022 Tidy up wire mock service hooks

--- src/SFA.DAS.Funding.SystemAcceptanceTests/Hooks/ServiceBusEndpointHook.cs
+++ src/SFA.DAS.Funding.SystemAcceptanceTests/Hooks/ServiceBusEndpointHook.cs
@@ -1,34 +1,8 @@
 ﻿using SFA.DAS.Funding.SystemAcceptanceTests.Helpers;
 using SFA.DAS.Funding.SystemAcceptanceTests.Infrastructure;
-using WireMock.Server;
 
 namespace SFA.DAS.Funding.SystemAcceptanceTests.Hooks
 {
-    [Binding]
-    public static class LocalWireMockServerHooks
-    {
-        private static WireMockServer? _wireMockServer;
-
-        [BeforeTestRun]
-        public static void StartLocalWireMockServer()
-        {
-            if (Configurator.EnvironmentName == "LOCAL")
-            {
-                var localWireMockUrl = new Uri(Configurator.GetConfiguration().WireMockBaseUrl);
-                if (localWireMockUrl.Host != "localhost")
-                    throw new ArgumentException("WireMockBaseUrl must be localhost when running tests against local environment.");
-
-                _wireMockServer = WireMockServer.Start(localWireMockUrl.Port);
-            }
-        }
-
-        [AfterTestRun]
-        public static void StopLocalWireMockServer()
-        {
-            _wireMockServer?.Stop();
-        }
-    }
-
     [Binding]
     public class ServiceBusEndpointHook
     {
