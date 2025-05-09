2b759ce85b15df6f45299fde8ec7137dd1ef0f08
tom-gough <tom.gough.tgsd@outlook.com>
2025-05-09 13:38:06 +0100

    FLP-1036 code review fixes

--- src/SFA.DAS.Funding.SystemAcceptanceTests/TestSupport/PaymentExpectation.cs
+++ src/SFA.DAS.Funding.SystemAcceptanceTests/TestSupport/PaymentExpectation.cs
@@ -2,10 +2,10 @@
 
 public class PaymentExpectation
 {
-	//When adding a property to this class update the assertion helper extension code - PaymentDeliveryPeriodExpectationExtensions
-	public decimal? Amount { get; set; }
-	public bool? SentForPayment { get; set; }
-	public Guid? EarningsProfileId { get; set; }
+    //When adding a property to this class update the assertion helper extension code - PaymentDeliveryPeriodExpectationExtensions
+    public decimal? Amount { get; set; }
+    public bool? SentForPayment { get; set; }
+    public Guid? EarningsProfileId { get; set; } 
     public decimal? ProviderIncentiveAmount { get; set; }
     public decimal? EmployerIncentiveAmount { get; set; }
 }
