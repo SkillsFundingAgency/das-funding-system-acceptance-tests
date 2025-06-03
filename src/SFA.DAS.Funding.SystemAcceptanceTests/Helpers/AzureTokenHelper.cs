32d77e4a7035d7850b47edca0f07bed062ec9e87
HussainsGitHub <85938019+HussainsGitHub@users.noreply.github.com>
2025-06-03 14:54:02 +0100

    DASD-13402 - Fix az login error

--- src/SFA.DAS.Funding.SystemAcceptanceTests/Helpers/AzureTokenHelper.cs
+++ src/SFA.DAS.Funding.SystemAcceptanceTests/Helpers/AzureTokenHelper.cs
@@ -8,7 +8,8 @@ public class AzureTokenHelper
     public async Task<string> GetAccessTokenAsync(string scope)
     {
         var credential = new DefaultAzureCredential();
-        var token = await credential.GetTokenAsync(new TokenRequestContext([scope]));
+        var token = await credential.GetTokenAsync(new TokenRequestContext(
+            new[] { $"{scope}/.default" }));
         return token.Token;
     }
 }
