57076f55d5f1dc8d43f09b6a1a65dc38ffc56f04
ShakilUmar <umar.shakil@education.gov.uk>
2025-10-27 15:18:25 +0000

    ++

--- src/SFA.DAS.Funding.SystemAcceptanceTests/Constants.cs
+++ src/SFA.DAS.Funding.SystemAcceptanceTests/Constants.cs
@@ -2,5 +2,5 @@
 
 internal static class Constants
 {
-    internal const long UkPrn = 88888882;// all tests use the same UKPRN
+    internal const long UkPrn = 88888888;// all tests use the same UKPRN
 }
