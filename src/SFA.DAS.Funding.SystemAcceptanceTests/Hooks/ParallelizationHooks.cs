8d41b4e5f46742deec1df718492def5c2a8a2660
tom-gough <tom.gough.tgsd@outlook.com>
2025-11-05 14:21:36 +0000

    FLP-1354 hooks

---
+++ src/SFA.DAS.Funding.SystemAcceptanceTests/Hooks/ParallelizationHooks.cs
@@ -0,0 +1,19 @@
+﻿namespace SFA.DAS.Funding.SystemAcceptanceTests.Hooks;
+
+[Binding]
+public class ParallelizationHooks
+{
+    private static readonly object _lock = new object();
+
+    [BeforeScenario("nonparallelizable")]
+    public void BeforeNonParallelScenario()
+    {
+        Monitor.Enter(_lock);
+    }
+
+    [AfterScenario("nonparallelizable")]
+    public void AfterNonParallelScenario()
+    {
+        Monitor.Exit(_lock);
+    }
+}
