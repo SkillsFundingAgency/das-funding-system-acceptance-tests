edd663fadbd3f94e302aee83e4cf118db5ece376
ShakilUmar <umar.shakil@education.gov.uk>
2025-12-15 16:39:28 +0000

    first test added

--- src/SFA.DAS.Funding.SystemAcceptanceTests/Helpers/IncentiveEarningsAssertionHelper.cs
+++ src/SFA.DAS.Funding.SystemAcceptanceTests/Helpers/IncentiveEarningsAssertionHelper.cs
@@ -11,15 +11,18 @@ public static class IncentiveEarningsAssertionHelper
     /// <param name="startDate">The start date of the apprenticeship.</param>
     /// <param name="incentiveNumber">The incentive number (1 or 2)</param>
     /// <param name="paymentType">The type of incentive to look for (ProviderIncentive or EmployerIncentive).</param>
+    /// <param name="dueDate">The date incentive is expected to be due (optional)</param>
+    /// <param name="breakInLearning">The date ranges for Break In Learnings of the episode (optional)</param>
     /// <returns>True if a matching incentive earning exists; otherwise, false.</returns>
-    public static bool IncentiveEarningExists(this List<AdditionalPaymentsModel> additionalPayments, DateTime startDate, byte incentiveNumber, AdditionalPaymentType paymentType, DateTime? dueDate = null)
+    public static bool IncentiveEarningExists(this List<AdditionalPaymentsModel> additionalPayments, DateTime startDate, byte incentiveNumber,
+        AdditionalPaymentType paymentType, DateTime? dueDate = null, List<EpisodeBreakInLearning>? breakInLearning = null)
     {
         if (incentiveNumber is not (1 or 2))
             throw new Exception("Assertion helper only supports incentive earning number of 1 or 2");
 
         var expectedPeriod = incentiveNumber == 2
-            ? startDate.AddDays(364).ToAcademicYearAndPeriod() // 365th day of learning
-            : startDate.AddDays(89).ToAcademicYearAndPeriod(); // 90th day of learning
+            ? AdjustForBreaks(startDate, 364, breakInLearning).ToAcademicYearAndPeriod() // 365th day of learning
+            : AdjustForBreaks(startDate, 89, breakInLearning).ToAcademicYearAndPeriod(); // 90th day of learning
 
         return additionalPayments.Any(x =>
             x.AcademicYear == expectedPeriod.AcademicYear
@@ -30,5 +33,23 @@ public static class IncentiveEarningsAssertionHelper
             && (dueDate == null || x.DueDate == dueDate));
     }
 
+    private static DateTime AdjustForBreaks(DateTime startDate, int milestoneDays, List<EpisodeBreakInLearning> breaksInLearning)
+    {
+        var incentiveDate = startDate.AddDays(milestoneDays);
 
+        if (breaksInLearning == null || breaksInLearning.Count == 0)
+            return incentiveDate;
+
+        foreach (var b in breaksInLearning.OrderBy(x => x.StartDate))
+        {
+            int duration = (b.EndDate - b.StartDate).Days + 1;
+
+            if (b.StartDate <= incentiveDate)
+            {
+                incentiveDate = incentiveDate.AddDays(duration);
+            }
+        }
+
+        return incentiveDate;
+    }
 }
