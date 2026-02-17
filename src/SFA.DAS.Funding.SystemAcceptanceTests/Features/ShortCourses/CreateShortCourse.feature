6382068c2c077e8ae186bb123fe42c4366b71acf
Chris Foster <chrisfoster186@googlemail.com>
2026-02-17 12:28:57 +0000

    Initial boilerplate

---
+++ src/SFA.DAS.Funding.SystemAcceptanceTests/Features/ShortCourses/CreateShortCourse.feature
@@ -0,0 +1,13 @@
+﻿Feature: CreateShortCourse
+
+SLD provides new information on Learners on Short Courses to a dedicated endpoint.
+Short Courses are stored in Learning.
+Approvals are notified via an event.
+Earnings are calculated (as part of a future story).
+
+@regression
+Scenario: Add a Short Course
+	When SLD inform us of a new Short Course
+	Then the Short Course details are recorded in Learning
+	And a LearnerData event is published to approvals
+
---
+++ src/SFA.DAS.Funding.SystemAcceptanceTests/StepDefinitions/ShortCourseStepDefinitions.cs
@@ -0,0 +1,25 @@
+﻿namespace SFA.DAS.Funding.SystemAcceptanceTests.StepDefinitions
+{
+    [Binding]
+    public class ShortCourseStepDefinitions(ScenarioContext scenarioContext)
+    {
+        [When(@"SLD inform us of a new Short Course")]
+        public void WhenSLDInformUsOfANewShortCourse()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"the Short Course details are recorded in Learning")]
+        public void ThenTheShortCourseDetailsAreRecordedInLearning()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"a LearnerData event is published to approvals")]
+        public void ThenALearnerDataEventIsPublishedToApprovals()
+        {
+            throw new PendingStepException();
+        }
+
+    }
+}
