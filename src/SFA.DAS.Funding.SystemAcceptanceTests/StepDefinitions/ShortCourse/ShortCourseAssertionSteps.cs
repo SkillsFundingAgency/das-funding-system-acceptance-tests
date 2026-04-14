cadce5dc25d738d5aa6e0b59d2d6dfaee48b550f
tom-gough <tom.gough@talentconsulting.uk>
2026-04-14 08:43:29 +0100

    fixes

--- src/SFA.DAS.Funding.SystemAcceptanceTests/StepDefinitions/ShortCourse/ShortCourseAssertionSteps.cs
+++ src/SFA.DAS.Funding.SystemAcceptanceTests/StepDefinitions/ShortCourse/ShortCourseAssertionSteps.cs
@@ -92,8 +92,8 @@ public class ShortCourseAssertionSteps(ScenarioContext context, LearnerDataOuter
         Assert.AreEqual(expectedCourse.ExpectedEndDate, episode.ExpectedEndDate, "ExpectedEndDate does not match.");
         Assert.AreEqual((byte)LearnerData.Events.LearningType.ApprenticeshipUnit, episode.LearningType, "LearningType does not match.");
 
-        if(testData.CommitmentsApprenticeshipCreatedEvent?.ApprenticeshipEmployerTypeOnApproval != null)
-            Assert.AreEqual(testData.CommitmentsApprenticeshipCreatedEvent.ApprenticeshipEmployerTypeOnApproval.ToString(), episode.FundingType, "EmployerType does not match.");
+        if(testData.IsShortCourseApproved && testData.CommitmentsApprenticeshipCreatedEvent?.ApprenticeshipEmployerTypeOnApproval != null)
+            Assert.AreEqual((byte)testData.CommitmentsApprenticeshipCreatedEvent.ApprenticeshipEmployerTypeOnApproval, episode.EmployerType, "EmployerType does not match.");
 
         var expectedLearningSupports = expectedCourse.LearningSupport ?? new();
         var actualLearningSupports = episode.LearningSupport ?? new();
